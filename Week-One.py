b045a7a6e3dd8e4a894d6314d5c0ce3c099d2610
Mykolas Perevicius <75288459+mykolas-perevicius@users.noreply.github.com>
2022-09-06 15:16:15 -0400

    Create Week-One.py

---
+++ Week-One.py
@@ -0,0 +1,19 @@
+https://replit.com/join/zzoajqfnqo-mykoalas
+
+#comment
+
+#comments can say literally anything because the code ignores it
+
+#list
+
+list_1 = ["the", "brown", "fox", 5, 5.5, True, None]
+
+#functions
+
+print(list_1)
+
+#  f(x) = y
+
+dictionary_1 = {"Pizza Slice" : 2.0, "Garlic Bread": 1.0} 
+
+print(dictionary_1)
