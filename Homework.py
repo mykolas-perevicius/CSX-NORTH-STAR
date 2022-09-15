ba291a9ce2e70c0a2bd012547964cc6918b9b3d1
Mykolas Perevicius <75288459+mykolas-perevicius@users.noreply.github.com>
2022-09-15 15:15:45 -0400

    Create Homework.py

---
+++ Homework.py
@@ -0,0 +1,4 @@
+# Set a variable called name equal to your name, then set a variable of age
+# equal to your age as an integer. Print a statement with an output of
+# "My name is [name here] and I am [age here] years old." using the above
+# variables in your print statement.
